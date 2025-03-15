c25a8fe41512875a75f5ffa8dba6a18bcb26a826
Okunlola Ridwat <okunlolaridwanat@gmail.com>
2025-03-15 23:14:47 +0100

    update

--- src/lib/authUtils.js
+++ src/lib/authUtils.js
@@ -3,15 +3,20 @@ import { auth } from "@clerk/nextjs/server";
 let cachedUser = null;
 
 export async function getCurrentUser() {
+  const { userId, sessionClaims } = await auth();
   if (!cachedUser) {
-    const { userId, sessionClaims } = await auth();
     cachedUser = {
       userId,
       role: sessionClaims?.metadata?.role || "guest", // Default to 'guest'
       sessionClaims,
     };
   }
-  return cachedUser;
+  // return cachedUser;
+  return {
+    userId,
+    role: sessionClaims?.metadata?.role || "guest", // Default to 'guest'
+    sessionClaims,
+  }
 }
 
 export async function getUserId() {
