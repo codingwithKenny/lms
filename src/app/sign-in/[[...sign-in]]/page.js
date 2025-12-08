3e19710a454ef4b56d74214211e197631dea7301
CodingwithKenny <okunlolaridwanat@gmail.com>
2025-12-09 00:52:57 +0100

    signin fix

--- src/app/sign-in/[[...sign-in]]/page.js
+++ src/app/sign-in/[[...sign-in]]/page.js
@@ -1,8 +1,7 @@
 "use client";
 
-import * as Clerk from "@clerk/elements/common";
-import * as SignIn from "@clerk/elements/sign-in";
-import { useUser, useAuth } from "@clerk/nextjs";
+import { SignIn, useUser, useAuth } from "@clerk/nextjs";
+
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
@@ -55,56 +54,8 @@ const LoginPage = () => {
   }
 
   return (
-    <div className="h-screen flex items-center justify-center bg-purple-50">
-      <SignIn.Root>
-        <SignIn.Step
-          name="start"
-          className="bg-white p-12 rounded-md shadow-2xl flex flex-col gap-2"
-        >
-          <h1 className="text-xl font-bold flex items-center gap-2">
-            <Image src="/logo.png" alt="" width={24} height={24} />
-            E - Portal
-          </h1>
-          <h2 className="text-gray-400">Sign in to your account</h2>
-          <Clerk.GlobalError className="text-sm text-red-400" />
-          <Clerk.Field name="identifier" className="flex flex-col gap-2">
-            <Clerk.Label className="text-xs text-gray-500">
-              Username
-            </Clerk.Label>
-            <Clerk.Input
-              type="text"
-              required
-              className="p-2 rounded-md ring-1 ring-gray-300"
-            />
-            <Clerk.FieldError className="text-xs text-red-400" />
-          </Clerk.Field>
-          <Clerk.Field name="password" className="flex flex-col gap-2 relative">
-            <Clerk.Label className="text-xs text-gray-500">Password</Clerk.Label>
-            <div className="relative">
-              <Clerk.Input
-                type={showPassword ? "text" : "password"}
-                required
-                className="p-2 pr-10 w-full rounded-md ring-1 ring-gray-300"
-              />
-              <button
-                type="button"
-                onClick={() => setShowPassword((prev) => !prev)}
-                className="absolute top-1/2 right-2 -translate-y-1/2 text-gray-500"
-              >
-                {showPassword ? <EyeOff size={16} /> : <Eye size={16} />}
-              </button>
-            </div>
-            <Clerk.FieldError className="text-xs text-red-400" />
-          </Clerk.Field>
-
-          <SignIn.Action
-            submit
-            className="bg-purple-500 text-white my-1 rounded-md text-sm p-[10px]"
-          >
-            Sign In
-          </SignIn.Action>
-        </SignIn.Step>
-      </SignIn.Root>
+     <div className="h-screen flex items-center justify-center bg-purple-50">
+      <SignIn signUpUrl={"/"} path="/sign-in" routing="path" />
     </div>
   );
 };
